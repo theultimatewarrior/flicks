7463247d582de5e186742a32c0f591f057e6986a
theultimatewarrior <mikenguyen191@gmail.com>
2013-11-03 03:25:47 -0500

    Project created

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>My Flicks</title>
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+<div data-role="page" id="main-page" data-theme="a">
+
+    <div data-role="content" data-theme="a">
+    
+    </div>
+</div>
+</body>
+</html>
