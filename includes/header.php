efbe3278184339cd5359102872f3637c7d0783aa
theultimatewarrior <mikenguyen191@gmail.com>
2013-11-09 18:42:38 -0500

    Added javascript file

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="css/main.css" type="text/css" />
     <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
     <script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+    <script src="js/script.js"></script>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Javascript
+
+function redirect_home() {
+    setTimeout("location.href = 'home.php';",1500);
+}
