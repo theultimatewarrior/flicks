8e01da9bcf8e144497f920c9e4487edf4696e3b2
theultimatewarrior <mikenguyen191@gmail.com>
2013-11-09 18:43:14 -0500

    Movie search implemented

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 ?>
     <div data-role="page" id="main-page">
         <div data-role="content">
-                <form id="main_search" action="search.php" method="GET">
-                    <div class="main_title">flicks</div>
-                    <input type="search" class="search_box" name="query" value="" data-mini="true" data-icon="search" data-iconpos="left" placeholder="search" />
-                </form>
+            <form id="main_search" action="search.php" method="GET">
+                <div class="main_title">flicks</div>
+                <input type="search" class="search_box" name="query"  data-mini="true" data-icon="search" data-iconpos="left" placeholder="search" />
+            </form>
         </div>
     </div>
 <?php include("includes/footer.php"); ?>
--- search.php
+++ search.php
@@ -1,15 +1,23 @@
 <?php
 	include("includes/init.php");
 	include('includes/header.php');
+    
+    $general->logged_out_protect();
+    
+    if (isset($_GET['query'])) {  
+        $query = $_GET['query'];
+        $query = ltrim($query);
+        $query = rtrim($query);
+    }
 ?>
     <div data-role="page" id="main-page" />
         <div data-role="content">
-            <?php 
-                if (isset($_GET['query'])) {  
-                    $query = $_GET['query'];
-                ?>
-            <p><?php echo $query; ?></p>
-            <?php } ?>
+            <div data-role="header" data-position="fixed">
+                <h1>Search: "<?php echo $query; ?>"</h1>
+            </div>
+            <ul data-role="listview" data-inset="false">
+                <?php $rtdb->search_movie($query); ?>
+            </ul>
         </div>
     </div>
 <?php include('includes/footer.php'); ?>
