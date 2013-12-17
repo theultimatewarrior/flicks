2d2e371436e3f965803d4c58c6294750e8a4103d
theultimatewarrior <mikenguyen191@gmail.com>
2013-12-17 16:59:31 -0500

    Logout Commit

--- logout.php
+++ logout.php
@@ -1,5 +1,15 @@
 <?php
 	require_once('includes/init.php');
+    
+    // Unset all session values
+    $_SESSION = array();
+    
+    // get session parameters 
+    $params = session_get_cookie_params();
+    
+    // Delete the actual cookie.
+    setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+    
 	session_destroy();
 	header('Location: index.php');
 ?>
